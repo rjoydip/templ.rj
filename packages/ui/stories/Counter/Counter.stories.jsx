50269167f0a6321100d1815df2eb24b1389f5e02
Joydip Roy <112172822+rjoydip@users.noreply.github.com>
2023-06-27 23:34:27 +0530

    Feature: added storybook for UI component (#14)
    * feat: added ui component basic setup and storybook setup
    
    * fix: ci issues
    
    * chore: pkg updated
    
    * fix: megalinter issues of trivy and djlint
    
    * docs: added megalinter badge and fix: typo
    
    * feat: added storybook in UI
    
    * chore: exclude storybook-static directory for DJLINT, HTMLHINT
    
    * chore: exclude storybook-static directory for EDITORCONFIG

--- packages/ui/stories/Counter/Counter.stories.jsx
+++ packages/ui/stories/Counter/Counter.stories.jsx
@@ -1,4 +1,4 @@
-import { Counter } from '../../components/Counter'
+import { Counter } from '../../src'
 
 export default {
   title: 'Components/Counter',
---
+++ packages/ui/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((options) => {
+  return {
+    entry: ['src/index.ts'],
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+    dts: true,
+    minify: !options.watch,
+    format: ['cjs', 'esm'],
+    external: ['preact'],
+  }
+})
