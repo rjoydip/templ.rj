72588c3a3fa38232c503da6bea1085cfff35bca8
Joydip Roy <joydipand@gmail.com>
2023-08-24 23:43:20 +0530

    chore: removed ts-check from docusaurus config file

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,10 +1,8 @@
-// @ts-check
-
 // Note: type annotations allow type checking and IDEs autocompletion
 // eslint-disable-next-line no-undef
-const lightCodeTheme = require('prism-react-renderer/themes/github')
+const lightCodeTheme = require('prism-react-renderer').themes.github
 // eslint-disable-next-line no-undef
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const darkCodeTheme = require('prism-react-renderer').themes.dracula
 
 const organizationName = 'rjoydip' // Usually your GitHub org/user name.
 const projectName = 'templ' // Usually your repo name.
