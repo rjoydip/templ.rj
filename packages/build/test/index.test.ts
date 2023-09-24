382470eaea85ebb6d66c51f2d49a360cb3c2040c
Joydip Roy <112172822+rjoydip@users.noreply.github.com>
2023-09-25 00:36:10 +0530

    chore: removed build package (#103)

--- packages/build/test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from 'vitest'
-import * as buildPackage from '../src'
-
-describe('@templ/build', () => {
-  test('should be exported build-package modules', () => {
-    expect(buildPackage).toBeDefined()
-  })
-})
