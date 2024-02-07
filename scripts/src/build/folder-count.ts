1f0995ff7cf0a777eeb8612f8c8176c479cf1bb0
Joydip Roy <joydipand@gmail.com>
2024-02-07 12:47:00 +0530

    🐛 fix(none): ui build issue and build folder count check

--- scripts/src/build/folder-count.ts
+++ scripts/src/build/folder-count.ts
@@ -4,17 +4,17 @@ import { root } from '../utils'
 
 export async function run() {
   const cwd = root
-  const ignorePatterns = ['.git/**', '**/node_modules/**', '*templ.mjs', '*.code-workspace']
+  const ignorePatterns = ['.git/**', '**/node_modules/**']
   const workingDirectories = await globby(['{packages,apps,plugins}/**/package.json'], {
     ignore: ignorePatterns,
     cwd,
   })
-  const files = await globby(['{packages,apps,plugins}/**/dist/index.{js,html}'], {
+  const files = await globby(['{packages,apps,plugins}/**/dist/index.{js,html}', '{packages,apps,plugins}/**/.next/package.json'], {
     ignore: ignorePatterns,
     cwd,
   })
 
-  files.length === workingDirectories.length ? consola.success('Dist count matched') : consola.error('Dist count not match')
+  files.length === workingDirectories.length ? consola.success('Build output count matched') : consola.error('Build output count not match')
 }
 
 run().catch(consola.error)
