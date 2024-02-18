6cabe20311e5a7c70c7f89c9fbc888deca3e55d6
Joydip Roy <joydipand@gmail.com>
2024-02-18 23:30:08 +0530

    🐛 fix(none): vitest shared in basic-js

--- generator/templates/basic-js/vitest.config.mjs
+++ generator/templates/basic-js/vitest.config.mjs
@@ -1,5 +1,5 @@
 import { defineProject, mergeConfig } from 'vitest/config'
-import configShared from '../../../config/vitest/shared'
+import configShared from '../../../.config/vitest.shared'
 
 export default mergeConfig(
   configShared,
