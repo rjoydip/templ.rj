ed4127b52813c1cc92a5cd94f40a6fd7197edda4
Joydip Roy <joydipand@gmail.com>
2024-02-26 14:09:42 +0530

    chore(test): removed ci check for github action

--- packages/config/test/load.test.ts
+++ packages/config/test/load.test.ts
@@ -3,7 +3,6 @@ import { resolve } from 'node:path'
 import { findWorkspaceDir } from 'pkg-types'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { z } from 'zod'
-import { isCI } from 'std-env'
 import { loadConfig } from '../src'
 
 describe('@templ/config > load', () => {
@@ -174,14 +173,8 @@ describe('@templ/config > load', () => {
         },
         flag: {},
       })
-      if (isCI) {
-        expect(process.env.DATABASE_URL).toBeUndefined()
-        expect(process.env.OPEN_AI_API_KEY).toBeUndefined()
-      }
-      else {
-        expect(process.env.DATABASE_URL).toBe('postgres//127.0.0.1:5432/mydb')
-        expect(process.env.OPEN_AI_API_KEY).toBe('OPEN_AI_API_KEY')
-      }
+      expect(process.env.DATABASE_URL).toBe('postgres//127.0.0.1:5432/mydb')
+      expect(process.env.OPEN_AI_API_KEY).toBe('OPEN_AI_API_KEY')
     })
 
     it('should load environment values from .env.local file', async () => {
@@ -198,14 +191,8 @@ describe('@templ/config > load', () => {
         },
         flag: {},
       })
-      if (isCI) {
-        expect(process.env.DATABASE_URL).toBeUndefined()
-        expect(process.env.OPEN_AI_API_KEY).toBeUndefined()
-      }
-      else {
-        expect(process.env.DATABASE_URL).toBe('postgres//127.0.0.1:5432/mydb')
-        expect(process.env.OPEN_AI_API_KEY).toBe('OPEN_AI_API_KEY')
-      }
+      expect(process.env.DATABASE_URL).toBe('postgres//127.0.0.1:5432/mydb')
+      expect(process.env.OPEN_AI_API_KEY).toBe('OPEN_AI_API_KEY')
     })
 
     it('should throw error when wrong value is being provided instead of right', async () => {
