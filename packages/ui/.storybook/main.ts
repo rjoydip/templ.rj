38b1376af715c95c71560c86213a935a62d2deec
Joydip Roy <joydipand@gmail.com>
2024-02-29 15:51:29 +0530

    chore: added addon-controls

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,13 +1,14 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../registry/**/*.mdx', '../registry/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
+    '@storybook/addon-controls',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
   ],
   framework: {
     name: '@storybook/react-vite',
